6a6d314e2443adfb9b17f8051ef17da78f804045
Tkhangds <gunnyvippro111@gmail.com>
2025-03-05 12:48:09 +0700

    feat: added Express.js and typing detection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-import { createServer } from "http";
 import { Server } from "socket.io";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const ioServer = new Server(httpServer, {
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+const ioServer = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
@@ -15,10 +28,35 @@ const ioServer = new Server(httpServer, {
 ioServer.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
+  // Upon connection - only to user
+
+  socket.emit("message", "Welcome to chatapp");
+
+  // Upong to user - to all other
+
+  socket.broadcast.emit(
+    "message",
+    `User ${socket.id.substring(0, 5)} connected`
+  );
+
+  // Listen for a message event
   socket.on("message", (data) => {
     console.log(data);
     ioServer.emit("message", `${socket.id.substring(0, 5)} : ${data}`);
   });
-});
 
-httpServer.listen(3500, () => console.log("listening on port 3500"));
+  // When user disconnects - to all other
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id.substring(0, 5)} disconnected`);
+    socket.broadcast.emit(
+      "message",
+      `User ${socket.id.substring(0, 5)} connected`
+    );
+  });
+
+  //Listen for activity
+
+  socket.on("activity", (name) => {
+    socket.broadcast.emit("activity", name);
+  });
+});
